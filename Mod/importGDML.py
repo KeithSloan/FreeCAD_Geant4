28b9bdfbb98ca2c6951a447856426e54b97e0e11
KeithSloan <keith@sloan-home.co.uk>
2018-12-12 21:24:32 +0000

    Initially just a copy of FreeCAD_Import_GDML

---
+++ Mod/importGDML.py
@@ -0,0 +1,252 @@
+# -*- coding: utf8 -*-
+
+#***************************************************************************
+#*                                                                         *
+#*   Copyright (c) 2017 Keith Sloan <keith@sloan-home.co.uk>               *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         * 
+#*   Acknowledgements :                                                    *
+#*                                                                         *
+#*                                                                         *
+#***************************************************************************
+__title__="FreeCAD - Geant4 GDML importer"
+__author__ = "Keith Sloan <keith@sloan-home.co.uk>"
+__url__ = ["https://github.com/KeithSloan/FreeCAD_Geant4"]
+
+printverbose = False
+
+import FreeCAD, os, sys, re, math
+import Part, PartGui
+
+if FreeCAD.GuiUp:
+    import FreeCADGui
+    gui = True
+else:
+    if printverbose: print("FreeCAD Gui not present.")
+    gui = False
+
+
+import Part
+
+
+if open.__module__ == '__builtin__':
+    pythonopen = open # to distinguish python built-in open function from the one declared here
+
+
+#try:
+#    _encoding = QtGui.QApplication.UnicodeUTF8
+#    def translate(context, text):
+#        "convenience function for Qt translator"
+#        from PySide import QtGui
+#        return QtGui.QApplication.translate(context, text, None, _encoding)
+#except AttributeError:
+#    def translate(context, text):
+#        "convenience function for Qt translator"
+#        from PySide import QtGui
+#        return QtGui.QApplication.translate(context, text, None)
+
+def open(filename):
+    "called when freecad opens a file."
+    global doc
+    global pathName
+    docname = os.path.splitext(os.path.basename(filename))[0]
+    doc = FreeCAD.newDocument(docname)
+    if filename.lower().endswith('.gdml'):
+        processGDML(filename)
+    return doc
+
+def insert(filename,docname):
+    "called when freecad imports a file"
+    global doc
+    global pathName
+    groupname = os.path.splitext(os.path.basename(filename))[0]
+    try:
+        doc=FreeCAD.getDocument(docname)
+    except NameError:
+        doc=FreeCAD.newDocument(docname)
+    if filename.lower().endswith('.gdml'):
+        processGDML(filename)
+
+class switch(object):
+    value = None
+    def __new__(class_, value):
+        class_.value = value
+        return True
+
+def case(*args):
+    return any((arg == switch.value for arg in args))
+
+def getVal(ptr,var) :
+    return float(eval(ptr.get(var)))
+
+def processPlacement(base,rot) :
+    print "Rotation : "
+    print rot.attrib
+    if 'y' in rot.attrib :
+	axis = FreeCAD.Vector(0,1,0) 
+        angle = float(rot.attrib['y'])
+    if 'x' in rot.attrib :
+	axis = FreeCAD.Vector(1,0,0) 
+        angle = float(rot.attrib['x'])
+    if 'z' in rot.attrib :
+	axis = FreeCAD.Vector(0,0,1) 
+        angle = float(rot.attrib['z'])
+    print angle 
+    place = FreeCAD.Placement(base,axis,angle)
+    return place
+
+def createBox(solid,volref,lx,ly,lz,rot) :
+    print "CreateBox : "
+    print solid.attrib
+    mycube=doc.addObject('Part::Box',volref.get('ref')+'_'+solid.get('name')+'_')
+    x = getVal(solid,'x')
+    y = getVal(solid,'y')
+    z = getVal(solid,'z')
+    mycube.Length = x
+    mycube.Width = y
+    mycube.Height = z
+    print "Logical Position : "+str(lx)+','+str(ly)+','+str(lz)
+    base = FreeCAD.Vector(lx-x/2,ly-y/2,lz-z/2)
+    mycube.Placement = processPlacement(base,rot)
+    print mycube.Placement.Rotation
+    mycube.ViewObject.DisplayMode = 'Wireframe'
+
+def makeCylinder(solid,r) :
+    mycyl = doc.addObject('Part::Cylinder',solid.get('name')+'_')
+    mycyl.Height = getVal(solid,'z')
+    mycyl.Radius = r
+    if solid.get('aunit' == 'rad') :
+       mycyl.Angle = 180 * float(solid.get('deltaphi')) / math.pi
+    if solid.get('aunit' == 'degrees') :
+       mycyl.Angle = solid.get('deltaphi')
+    return mycyl
+
+def createTube(solid,volref,lx,ly,lz,rot) :
+    print "CreateTube : "
+    print solid.attrib
+    rmax = getVal(solid,'rmax')
+    rmin = solid.get('rmin')
+    if ( rmin is None or rmin == 0 ) :
+       mytube = makeCylinder(solid,rmax)
+    else :
+       mytube = doc.addObject('Part::Cut','Tube_'+solid.get('name')+'_')
+       mytube.Base = makeCylinder(solid,rmax)
+       mytube.Tool = makeCylinder(solid,rmin)
+
+    print "Position : "+str(lx)+','+str(ly)+','+str(lz)
+    base = FreeCAD.Vector(lx,ly,lz)
+    mytube.Placement = processPlacement(base,rot)
+    print mytube.Placement.Rotation
+
+def createCone(solid,volref,lx,ly,lz,rot) :
+    print "CreateCone : "
+    print solid.attrib
+
+def createSolid(solid,volref,lx,ly,lz,rot) :
+    while switch(solid.tag):
+        if case('box'):
+           createBox(solid,volref,lx,ly,lz,rot) 
+           break
+        if case('tube'):
+           createTube(solid,volref,lx,ly,lz,rot) 
+           break
+        if case('cone'):
+           createCone(solid,volref,lx,ly,lz,rot) 
+           break
+        print "Solid : "+solid.tag+" Not yet supported"
+        break
+
+
+def getRef(ptr) :
+    ref = ptr.get('ref')
+    print "ref : " + ref
+    return ref
+
+def parseObject(root,ptr) :
+    print ptr.tag
+    print ptr.attrib
+    if ptr.tag in ["subtraction","union","intersection"] :
+       print "Boolean : "+ptr.tag
+       base = ptr.find('first')
+       name = getRef(base)
+       base = root.find("solids/*[@name='%s']" % name )
+       parseObject(root,base)
+       tool = ptr.find('second')
+       name = getRef(tool)
+       tool = root.find("solids/*[@name='%s']" % name )
+       parseObject(root,tool)
+
+def getVolSolid(root,name):
+    print "Get Volume Solid"
+    vol = root.find("structure/volume[@name='%s']" % name )
+    sr = vol.find("solidref")
+    print sr.attrib
+    name = getRef(sr)
+    solid = root.find("solids/*[@name='%s']" % name )
+    return solid
+
+def parsePhysVol(root,ptr,lx,ly,lz):
+    print "ParsePhyVol"
+    pos = ptr.find("positionref")
+    if pos is not None :
+       name = getRef(pos)
+       pos = root.find("define/position[@name='%s']" % name )
+       print pos.attrib
+    else :
+       pos = ptr.find("position")
+    lx += float(eval(pos.get('x')))
+    ly += float(eval(pos.get('y')))
+    lz += float(eval(pos.get('z')))
+    rot = ptr.find("rotationref")
+    if rot is not None :
+       name = getRef(rot)
+       rot = root.find("define/rotation[@name='%s']" % name )
+    else :
+       rot = ptr.find("rotation")
+    volref = ptr.find("volumeref")
+    name = getRef(volref)
+    solid = getVolSolid(root,name)
+    if ((pos is not None) and (rot is not None)) :
+       createSolid(solid,volref,lx,ly,lz,rot)
+    parseVolume(root,name,lx,ly,lz)
+
+# ParseVolume 
+def parseVolume(root,name,lx,ly,lz) :
+    print "ParseVolume : "+name
+    vol = root.find("structure/volume[@name='%s']" % name )
+    print vol.attrib
+    for pv in vol.findall('physvol') : 
+        parsePhysVol(root,pv,lx,ly,lz)
+
+def processGDML(filename):
+    FreeCAD.Console.PrintMessage('Geant4 - Import GDML file : '+filename+'\n')
+    if printverbose: print ('Geant4 - ImportGDML Version 0.1')
+
+    import xml.etree.ElementTree as ET
+    tree = ET.parse(filename)
+    root = tree.getroot()
+ 
+    for setup in root.find('setup'):
+        print setup.attrib
+        ref = getRef(setup)
+        parseVolume(root,ref,0,0,0)
+
+    doc.recompute()
+    if printverbose:
+        print('End ImportGDML')
+    FreeCAD.Console.PrintMessage('End processing GDML file\n')
