02e00b06c6d2fe3f9c40646624c32fc951dfb9c1
KeithSloan <keith@sloan-home.co.uk>
2018-12-22 18:01:26 +0000

    Proper Scan

--- scanGDML.py
+++ scanGDML.py
@@ -15,33 +15,68 @@ class switch(object):
 def case(*args):
     return any((arg == switch.value for arg in args))
 
-def createBox(solid) :
-    print("Create Box : \n")
+def createBox(solid,x,y,z) :
+    print("Create Box : x "+str(x)+" y "+str(y)+" z "+str(z))
 
-def browsePhysicalVolume(LV):
-    num = LV.GetNoDaughters()
-    print("Logical Volume  : "+str(LV.GetName())+'\n')
-    print("Num Daughters : "+str(num)+'\n')
-    for i in range(1,num,1) :
-       pv  = LV.GetDaughter(i)
-       lvd = pv.GetLogicalVolume()
-       if ( lvd.GetNoDaughters() == 0) :
-          print("Logical Volume  : "+str(lvd.GetName())+'\n')
+def createCons() :
+    print("Create Cons ")
 
-          print("Parse Logical Volume \n")
-#          print dir(lvd)
-          solid = lvd.GetSolid()
-          while switch(type(solid)) :
-              if case(Geant4.G4geometry.G4Box):
-		 createBox(solid)
-		 break
+def createTrap(Z,Y1,X1,X2,X3,X4,TA1,TA2) :
+    print("Create Trap : z "+str(Z)+" Y1 "+str(Y1)+" X1 "+str(X1)
+	   +" X2 "+str(X2)+" X3 "+str(X3)+" X4 "+str(X4)+" TA1 "
+           +str(TA1)+" TA2 "+str(TA2))
 
-	      print "Solid type : "+str(type(solid))+" Not yet supported"
+def createTubs() :
+    print("Create Tubs ")
+
+def parseLogicalVolume(lv) :
+    print("Parse Logical Volume "+str(lv.GetName()))
+#   print dir(lv)
+    solid = lv.GetSolid()
+    while switch(type(solid)) :
+       if case(Geant4.G4geometry.G4Box):
+	      X = solid.GetXHalfLength()*2
+              Y = solid.GetYHalfLength()*2
+	      Z = solid.GetZHalfLength()*2
+ 	      createBox(solid,X,Y,Z)
+	      break
+      
+       if case(Geant4.G4geometry.G4Cons):
+              createCons()
               break
-          
-#          G4type = solid.GetEntityType()
-#          print("Solid : "+str(G4String(G4type))+" : "+entity+"\n")
+ 
+       if case(Geant4.G4geometry.G4Tubs):
+              createTubs()
+              break
 
+       if case(Geant4.G4geometry.G4Trap):
+ 	      Z  = solid.GetZHalfLength()*2
+              Y1 = solid.GetHalfLength1()*2 
+	      X1 = solid.getHalfLength1()*2
+	      X2 = solid.getHalfLength2()*2
+	      X3 = solid.getHalfLength3()*2
+	      X4 = solid.getHalfLength4()*2
+	      TA1 = solid.getTanAlpha1()
+	      TA2 = solid.getTanAlpha2()
+              creatTrap(Z,Y1,X1,X2,X3,X4,TA1,TA2)
+	      break
+
+       print "Solid type : "+str(type(solid))+" Not yet supported\n"
+       break
+          
+
+
+def browsePhysicalVolume(pv):
+    print("\nPhysical Volume  : "+str(pv.GetName()))
+    lv = pv.GetLogicalVolume()
+    num = lv.GetNoDaughters()
+    if num == 0 :
+       parseLogicalVolume(lv)
+    else :
+       print("Num Daughters : "+str(num))
+       for i in range(0,num,1) :
+           pvd = lv.GetDaughter(i)
+           browsePhysicalVolume(pvd)
 
 def main() :
     print "Main"
@@ -50,12 +85,16 @@ def main() :
 
 #    Reader = gdml_parser.GetReader()
 
-#    WV = gdml_parser.GetWorldVolume(G4String(str("Default")))
-    WV = gdml_parser.GetWorldVolume()
-    LV = WV.GetLogicalVolume()
+#    wv = gdml_parser.GetWorldVolume(G4String(str("Default")))
+    wv = gdml_parser.GetWorldVolume()
+    #LV = WV.GetLogicalVolume()
 
-    num = browsePhysicalVolume(LV)
+    num = browsePhysicalVolume(wv)
 
-filename = "/home/keith/GDML-Test-Files/test4.gdml"
+#filename = "/home/keith/GDML-Test-Files/lhcbvelo.gdml"
+#filename = "/home/keith/GDML-Test-Files/test4.gdml"
+#filename = "/home/keith/GDML-Test-Files/test5.gdml"
+filename = "/home/keith/GDML-Test-Files/test6.gdml"
+
 main()
 
