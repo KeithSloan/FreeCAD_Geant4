dc1bd6edae2b156e0f9273c85bf66430edf9b5ab
KeithSloan <keith@sloan-home.co.uk>
2019-01-17 21:01:09 +0000

    Start fc2G4_Matrix

---
+++ myg4py/fc2G4_Matrix.cc
@@ -0,0 +1,7 @@
+//**********************************************************
+//  Shared library to create FC transfrom matrix from G4
+//       rotation matrix and translation vector
+//        
+//  Copyright : Keith Sloan
+//********************************************************** 
+#include <fc2G4_Matrix.hh>
---
+++ myg4py/fc2G4_Matrix.hh
@@ -0,0 +1,4 @@
+//**********************************************************
+//  Shared library to create FC transfrom matrix from G4
+//       rotation matrix and translation vector
+//********************************************************** 
